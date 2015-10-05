f16b9c9b4ef24d93bf5a03ecc48ba05b71adfd7f
Ross H. Anderson <ross.anderson@ualberta.ca>
2015-10-05 13:14:32 -0600

    Add GPLv3 LICENSE and Copyright header to all .java files

--- app/src/main/java/ca/rossanderson/rhanders_reflex/GameShowActivity.java
+++ app/src/main/java/ca/rossanderson/rhanders_reflex/GameShowActivity.java
@@ -1,3 +1,21 @@
+/*
+ * Reflex - Reaction Timer and Game Show Buzzer
+ * Copyright (C) 2015 Ross Anderson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ca.rossanderson.rhanders_reflex;
 
 import android.support.v7.app.AppCompatActivity;
